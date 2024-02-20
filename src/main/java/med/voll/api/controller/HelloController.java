b44c7ce0bce6a9239003da0c58fdaecf14b37e5b
Daniel Adorno Gomes <32546263+adornogomes@users.noreply.github.com>
2024-02-20 11:44:59 +0000

    Update HelloController.java

--- src/main/java/med/voll/api/controller/HelloController.java
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -2,19 +2,23 @@ package med.voll.api.controller;
 
 import med.voll.api.service.CloudStorageManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.HttpHeaders;
 
-import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.Storage.BlobTargetOption;
-import com.google.cloud.storage.Storage.PredefinedAcl;
-import com.google.cloud.storage.StorageOptions;
 
 import java.io.IOException;
+import java.nio.file.Files;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
@@ -56,11 +60,63 @@ public class HelloController {
         return cloudStorageManagerService.downloadObject(bucketName, objectName, filePath);
     }
 
-    @RequestMapping("/downloadFolder")
+    /*@RequestMapping("/downloadFolder")
     public String downloadFolder(@RequestParam(name = "bucketName") String bucketName,
-                                  @RequestParam(name = "objectName") String objectName,
-                                  @RequestParam(name = "filePath") String filePath) throws IOException {
+                                  @RequestParam(name = "folderName") String folderName) throws IOException {
+                                 // @RequestParam(name = "filePath") String filePath) throws IOException {
 
-        return cloudStorageManagerService.downloadFolder(bucketName, objectName, filePath);
+        return cloudStorageManagerService.downloadFolder(bucketName, folderName);
+    }*/
+
+    @RequestMapping("/downloadFolderAsZip")
+    public void downloadFolderAsZip(@RequestParam(name = "bucketName") String bucketName,
+                                 @RequestParam(name = "folderName") String folderName,
+                                 @RequestParam(name = "filePath") String filePath) throws IOException {
+
+        cloudStorageManagerService.downloadFolderAsZip(bucketName, folderName, filePath);
+    }
+    @GetMapping(path = "/file", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<InputStreamResource> getSpecificFile(@RequestParam(name = "bucketName") String bucketName,
+                                                               @RequestParam(name = "objectName") String objectName) {
+
+        try {
+            String contentType = Files.probeContentType(Paths.get(objectName));
+
+            return ResponseEntity.ok().contentType(MediaType.valueOf(contentType))
+                    .header("Content-Disposition", "attachment; filename=" + objectName)
+                    .body(new InputStreamResource(cloudStorageManagerService.getSpecificFile(bucketName, objectName)));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body(null);
+        }
+    }
+
+    @GetMapping(path = "/downloadFolder", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<Resource> downloadFolder(@RequestParam(name = "bucketName") String bucketName,
+                                                   @RequestParam(name = "folderName") String folderName) {
+
+        try {
+            Path zipFilePath = cloudStorageManagerService.downloadFolder(bucketName, folderName);
+            byte[] zipFileBytes = Files.readAllBytes(zipFilePath);
+            Files.delete(zipFilePath);
+
+            // Create a ByteArrayResource to wrap the byte array
+            ByteArrayResource resource = new ByteArrayResource(zipFileBytes);
+
+            // Set the response headers
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            headers.setContentDispositionFormData("attachment", folderName + ".zip");
+
+            // Return the zip file as a resource in the response entity
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentLength(zipFileBytes.length)
+                    .body(resource);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body(null);
+        }
     }
 }
