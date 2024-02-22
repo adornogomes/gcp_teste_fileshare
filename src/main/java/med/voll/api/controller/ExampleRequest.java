d9c4557e2b21bd7ed094e537cd60e42c9709c9ab
Daniel Adorno Gomes <32546263+adornogomes@users.noreply.github.com>
2024-02-22 09:20:03 +0000

    Add files via upload

---
+++ src/main/java/med/voll/api/controller/ExampleRequest.java
@@ -0,0 +1,14 @@
+package med.voll.api.controller;
+
+import lombok.Getter;
+
+import java.util.List;
+@Getter
+public class ExampleRequest {
+
+        private String bucketName;
+        private List<String> objectNames;
+
+        // Getters and setters
+
+}
